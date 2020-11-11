323c3df458fbe55bbb61f074ab9178ac886e413c
unknown <ambarki.youssef@gmail.com>
2020-11-11 09:44:49 +0100

    -top-navigation empty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent
   ],
   imports: [
     BrowserModule
